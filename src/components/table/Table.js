3dbd27f4308a3192f366938361fb2b39d3d5a34a
Ivan <ifabrichnov@yandex.ru>
2021-01-29 18:35:05 +0300

    Finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -8,9 +8,11 @@ import {TableSelection} from '@/components/table/TableSelection'
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listeners: ['mousedown', 'keydown']
+      name: 'Table',
+      listeners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
   }
 
@@ -26,7 +28,20 @@ export class Table extends ExcelComponent {
     super.init()
 
     const $cell = this.$root.find('[data-id="0:0"]')
+    this.selectCell($cell);
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text);
+    });
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus();
+    });
+  }
+
+  selectCell($cell) {
     this.selection.select($cell)
+    this.$emit('table:select', $cell);
   }
 
   onMousedown(event) {
@@ -36,7 +51,7 @@ export class Table extends ExcelComponent {
       const $target = $(event.target)
       if (event.shiftKey) {
         const $cells = matrix($target, this.selection.current)
-            .map(id => this.$root.find(`[data-id="${id}"]`))
+          .map(id => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
       } else {
         this.selection.select($target)
@@ -60,7 +75,11 @@ export class Table extends ExcelComponent {
       event.preventDefault()
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
+      this.selectCell($next); 
     }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target));
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,23 +3,40 @@ import {DomListener} from '@core/DomListener'
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
-    this.name = options.name || ''
+    this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
 
     this.prepare()
   }
 
+  //Настраиваю компонент до init
   prepare() {}
 
-  // Возвращает шаблон компонента
+  //Возвращает шаблон компонента
   toHTML() {
     return ''
   }
 
-  init() {
-    this.initDOMListeners()
+  //Уведомление слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
   }
 
+  //Подписка на событие event
+  $on(event, fn) {
+   const unsub = this.emitter.subscribe(event, fn);
+   this.unsubscribers.push(unsub);
+  }
+
+  //Инициализация компонент
+  init() {
+    this.initDOMListeners();
+  }
+
+  //удаление компонент
   destroy() {
-    this.removeDOMListeners()
+    this.removeDOMListeners();
+    this.unsubscribers.forEach(unsub => unsub());
   }
 }
